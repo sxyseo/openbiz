7f6fd14fe80c1b8dc5b7e6f8a352c8cc1a0de439
Jixian Wang <jixian@pickatale.com>
2014-01-14 16:05:24 +0700

    update ui

--- ui/objects/MiddleWareRouter.js
+++ ui/objects/MiddleWareRouter.js
@@ -4,48 +4,58 @@ define( ["objects/Router"], function(router){
 		_middlewaresRegExps : {},
 		_middlewaresCallbacks : {},
 		middlewares:{			
+		},		
+		_bindRoutes:function(){
+			openbiz.Router.prototype._bindRoutes.apply(this, arguments);
+			this._bindMiddlewares.call(this);
 		},
-		routes:{
-			""	: "middlewares"
+		_bindMiddlewares:function(){
+			if (!this.middlewares) return;
+		      this.middlewares = _.result(this, 'middlewares');
+		      var route, routes = _.keys(this.middlewares);
+		      while ((route = routes.pop()) != null) {
+		        this.middleware(route, this.middlewares[route]);
+		      }
 		},
-		route: function(route, name, callback) {
-	        var router = this;	        
-	       	var fragment = Backbone.history.getHash();	        
-	        var stack = [];
-	        for(var routeTest in this._middlewaresRegExps)
-	        {	        
-	        	if(this._middlewaresRegExps[routeTest].test(fragment)){
-                    stack.push(this[this._middlewaresCallbacks[routeTest]]);
-	        	}
+		middleware:function(route, name, callback){
+		      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+		      if (_.isFunction(name)) {
+		        callback = name;
+		        name = '';
+		      }
+		      if (!callback) callback = this[name];
+		      var router = this;
+		      Backbone.history.middleware(route, function(fragment,next) {
+		        var args = router._extractParameters(route, fragment);
+		        if(callback) { 
+		        	args.unshift(next);
+		        	callback.apply(router,args);
+		        }
+		        router.trigger.apply(router, ['route-middleware:' + name].concat(args));
+		        router.trigger('route-middleware', name, args);
+		        Backbone.history.trigger('route-middleware', router, name, args);
+		      });
+		      return this;
+		},
+		loadUrl: function(fragment) {
+	      fragment = Backbone.history.getFragment(fragment);
+	      var matchCounter = 0;
+	      return _.any(Backbone.history.handlers, function(handler) {	      	
+	        if (handler.route.test(fragment)) {	        	
+	        	if(matchCounter>=1){
+		          handler.callback(fragment);
+		          return true;
+		      	}
+		      	matchCounter++;
 	        }
-
-            var done = function(){
-	        	router.navigate(fragment, {trigger: true, replace: true});
-	        };
-	        if(stack.length){
-		        var hopCounter = 0 ;
-		        var nextHop = function()
-		        {
-                    hopCounter++;
-                    if(hopCounter >= stack.length){
-                        done.call(router);
-		        	}else{
-		        		stack[hopCounter].call(router,nextHop);
-		        	}
-                };
-				stack[hopCounter].call(router,nextHop);
-			}else{
-				done.call(router);
-			}
-	        
-	    },
-		constructor:function(){
+	      });
+	    },	    
+		constructor:function(){					
 			for(var route in this.middlewares){
 				this._middlewaresRegExps[route] = this._routeToRegExp(route);
 				this._middlewaresCallbacks[route] = this.middlewares[route];
-			}
-            router.prototype.initialize.apply(this,arguments);
-			router.apply(this, arguments);
+			}					
+			router.apply(this);		                  									
 		}
 	});
 });
---
+++ ui/utils/HistoryInit.js
@@ -0,0 +1,49 @@
+define(function(){
+	return function(){
+		if(Backbone.History.prototype._openbizInited != true){			
+			Backbone.History.prototype._loadUrl = Backbone.History.prototype.loadUrl;
+			_.extend(Backbone.History.prototype,{
+				_openbizInited:true,
+				middlewares:[],
+				middleware:function(route, callback) {
+			      this.middlewares.push({route: route, callback: callback});
+			    },
+				loadUrl: function(fragment) {
+			      fragment = this.fragment = this.getFragment(fragment);
+			      var matchedMiddlewares = [];
+			      _.any(this.middlewares, function(middlewares) {
+			      	if (middlewares.route.test(fragment)) {
+			      		matchedMiddlewares.push(middlewares.callback);
+			      	}			      	
+			      });			      
+				  if(matchedMiddlewares.length>0){
+				  	 this.processMiddlewares(fragment,matchedMiddlewares);
+				  }else{
+					 this._loadUrl.call(this,fragment);
+				  }			      
+			    },
+			    processMiddlewares:function(fragment,middlewares){
+			    	var history = this;
+					var done = function(fragment){										
+			        	history._loadUrl.call(history,fragment);
+			        };
+			        if(middlewares.length){
+				        var hopCounter = 0 ;
+				        var nextHop = function()
+				        {
+		                    hopCounter++;
+		                    if(hopCounter >= middlewares.length){
+		                        done.call(history,fragment);
+				        	}else{
+				        		middlewares[hopCounter](fragment,nextHop);
+				        	}
+		                };
+						middlewares[hopCounter](fragment,nextHop);
+					}else{
+						done.call(history,fragment);
+					}
+			    }
+			});
+		}		
+	}
+});
