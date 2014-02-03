22c12edb1899f7dc2c5725668fe872ee52ea3a35
Jixian Wang <jixian@pickatale.com>
2014-02-03 10:42:44 +0700

    update ui

--- openbiz-ui.js
+++ openbiz-ui.js
@@ -2,7 +2,8 @@
 var path = require('path'),
 	express = require('express'),
 	fs = require('fs'),
-	crypto = require('crypto');
+	crypto = require('crypto'),
+	caches = [];
 
 module.exports = function(app){
 	return {
@@ -20,36 +21,48 @@ module.exports = function(app){
 		getOpenbizUI:function(){
 			var self = this;
 			return function(req,res){
-				switch(process.env.NODE_ENV){
-				  case "development":
-				  default:
-				    var uiFile = "main.js"
-				    break;
-				  case "production":  
-				    var uiFile = "main.min.js"
-				    break;
+				if(caches.hasOwnProperty('main.js') && process.env.NODE_ENV=='production'){					
+					res.set('Content-Type','application/javascript');
+					res.send(200,caches['main.js']);
+				}else{
+					switch(process.env.NODE_ENV){
+					  case "development":
+					  default:
+					    var uiFile = "main.js"
+					    break;
+					  case "production":  
+					    var uiFile = "main.min.js"
+					    break;
+					}
+					var uiData = fs.readFileSync(path.join(__dirname,'ui',uiFile));
+					uiData = "var openbizUrl = '"+self.libUrl+"';\n" + uiData;
+					caches['main.js'] = uiData;
+					res.set('Content-Type','application/javascript');
+					res.send(200,uiData);
 				}
-				var uiData = fs.readFileSync(path.join(__dirname,'ui',uiFile));
-				uiData = "var openbizUrl = '"+self.libUrl+"';\n" + uiData;				
-				res.set('Content-Type','application/javascript');
-				res.send(200,uiData);
 			}
 		},
 		getOpenbizCSS:function(){
 			var self = this;
 			return function(req,res){
-				switch(process.env.NODE_ENV){
-				  case "development":
-				  default:
-				    var uiFile = "openbiz.css"
-				    break;
-				  case "production":  
-				    var uiFile = "openbiz.min.css"
-				    break;
+				if(caches.hasOwnProperty('openbiz.css') && process.env.NODE_ENV=='production'){
+					res.set('Content-Type','application/javascript');
+					res.send(200,caches['openbiz.css']);
+				}else{
+					switch(process.env.NODE_ENV){
+					  case "development":
+					  default:
+					    var uiFile = "openbiz.css"
+					    break;
+					  case "production":  
+					    var uiFile = "openbiz.min.css"
+					    break;
+					}
+					var uiData = fs.readFileSync(path.join(__dirname,'ui',"vendor","openbiz",uiFile));
+					caches['openbiz.css'] = uiData;
+					res.set('Content-Type','text/css');
+					res.send(200,uiData);
 				}
-				var uiData = fs.readFileSync(path.join(__dirname,'ui',"vendor","openbiz",uiFile));
-				res.set('Content-Type','text/css');
-				res.send(200,uiData);
 			}
 		}
 	};
