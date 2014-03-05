57e580706d330dd854f2cf40d1f1acb9cfcc7af5
Jixian Wang <jixian@pickatale.com>
2014-03-05 15:34:46 +0800

    update ui

--- ui/objects/View.js
+++ ui/objects/View.js
@@ -24,21 +24,44 @@ define(function(){
 			this.locale.baseUrl = this.app.baseUrl;
 			return this;
 		},
-		switchView:function(viewName,args){
+		switchView:function(viewName){
 			if(this.app==null)return;
-			var self=this;
+	    	var callback,args=[],self=this;;
+	    	switch(arguments.length){
+	    		case 2:
+	    			if(typeof arguments[1]=='function'){
+	    				callback = arguments[1];	
+	    			}else if(typeof arguments[1]=='object'){
+	    				args = arguments[1];
+	    			}
+	    			break;
+	    		case 3:
+	    			args = arguments[1];
+	    			callback = arguments[2];
+	    			break;
+	    	}
 	    	var viewArr = viewName.split(".");
 	    	var viewPath = "./modules/"+viewArr[0]+"/views/"+viewArr[1];
 	    	$(self.el).fadeOut(function(){
-				this.app.require([viewPath],function(targetView){
-					self.undelegateEvents();
-					var view = new targetView(args);
-					view.render();
-					openbiz.views._renderred[viewName] = view;
-					openbiz.views._inited[viewName] = false;
-					openbiz.ui.update($(self.el));
-					$(self.el).fadeIn();
-				})
+				self.undelegateEvents();
+				if(self.app.views.isRenderred(viewName)){
+					self.app.views.get(viewName).undelegateEvents();
+				}
+	            self.app.views.render(viewName,args,function(view){
+	            	var viewHTML = $(view.$el);
+	            	
+	            	view.delegateEvents();
+	                self.$el.replaceWith(viewHTML);
+	                try{
+		                self.on('hidden.bs.modal',function(){
+		                	view.undelegateEvents();
+		                });
+	            	}catch(e){};
+	                openbiz.ui.update($(self.$el));
+	                if(typeof callback =='function'){
+	                    callback();
+	                }
+	            });
 			});	
 		},
 		popupView:function(viewName){
@@ -64,6 +87,7 @@ define(function(){
 			}
             self.app.views.render(viewName,args,function(view){
             	var $modal = $(view.$el);
+            	view.delegateEvents();
                 $modal.modal();
                 $modal.on('hidden.bs.modal',function(){
                 	view.undelegateEvents();
