1260dcb0808334d469c6d2a7b21e6f8540359879
Jixian Wang <jixian@pickatale.com>
2014-04-14 14:19:49 +0800

    bugfix

--- ui/views/elements/forms/DropdownView.js
+++ ui/views/elements/forms/DropdownView.js
@@ -24,7 +24,7 @@ define(['./OptionElement'],function(element){
 							select += "<option value='"+display+"' data-value-field='"+display+"' data-display-field='"+display+"'>"+display+"</option>";
 						}
 					}
-					select += "</select>";
+					select += "</select><div id='select-com-error'></div>";
 					$(self.parent.el).find("."+self._selector).append(data+select);
 					$('.selectpicker').selectpicker();
 				}else if(self._modelType == "model"){
